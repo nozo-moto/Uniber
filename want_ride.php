07670794951dd4d8a0c5eff6529a25371a0c3e8a
Your Name <nozomotoitech@gmail.com>
2017-10-28 23:48:16 +0900

    add want_ride.php

--- want_ride.php
+++ want_ride.php
@@ -1,28 +1,25 @@
 <?php
-$db = new PDO('sqlite:db/uniber.db');
+try{
+    $db = new PDO('sqlite:db/uniber.db');
+    $json_string = file_get_contents('php://input');
+    $obj = json_decode($json_string);
+    foreach($obj as $o){
+        $uid = $obj->id;
+        $res_time = $obj->res_time;
+        $res_latitude = $obj->res_latitude;
+        $res_longitude = $obj->res_longitude;
+        $comment = $obj->comment;
+    }
 
-$json_test = [
-    "id" => "2",
-    "res_time" => "201710100910",
-    "res_latitude" => "135.00",
-    "res_longitude" => "35",
-    "comment" => "it's a comment"
-];
-$json_string = json_encode($json_test);
-
-// $json_string = file_get_contents('php://input')
-$obj = json_decode($json_string);
-
-foreach($obj as $o){
-    $uid = $obj->id;
-    $res_time = $obj->res_time;
-    $res_latitude = $obj->res_latitude;
-    $res_longitude = $obj->res_longitude;
-    $comment = $obj->comment;
+    $run = $db->query("insert into want_ride(uid, res_time, res_latitude, res_longitude) values('$uid' ,'$res_time' ,'$res_latitude' ,'$res_longitude' )");
+    if (!$run){
+        print("err "."\n");
+        return;
+    }
+    echo json_encode(array("result" => 1));
 }
-$run=$db->prepare("insert into want_ride(uid, res_time, res_latitude, res_longitude, comment) values('1' ,'201710100910' ,'135.00' ,'35' ,'it's a comment' )");
-if (!$run){
-    print("err "."\n");
-    return;
+catch (Exception $e){
+    echo json_encode(array("result" => 0));
 }
+
 ?>
