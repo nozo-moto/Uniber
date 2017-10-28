8bcb4d173283f3c7d7d54da547872934f27f04fc
Y.N <s1240095@gmail.com>
2017-10-28 17:25:24 +0900

    Add register

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = array('This','is','a','test','hello','world');
+echo(json_encode($a));
+echo "\n";
+$d = array('foo' => 'bar', 'baz' => 'long');
+echo(json_encode($d));
+echo "\n";
+?>
