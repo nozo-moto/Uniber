27d09a8d072cd0b596db7aefe100d5b0fd4ef2d8
Y.N <s1240095@gmail.com>
2017-10-29 00:20:27 +0900

    Refactor func_conv_addr

--- func_conv_addr.php
+++ func_conv_addr.php
@@ -1,27 +1,11 @@
 <?php
- 
-/*
-** 緯度経度取得及び設定
-*/
 
+//座標値を住所に変換
 function conv_addr($latitude,$longitude){
+$GMAD = json_decode(@file_get_contents('http://maps.google.com/maps/api/geocode/json?latlng=' . 
+    $latitude . ',' . $longitude . '&sensor=false&language=ja'), true);
 
- 
-/*
-** jsonデータ取得
-*/
- 
- $GMAD = json_decode(@file_get_contents('http://maps.google.com/maps/api/geocode/json?latlng=' . 
-        $latitude . ',' . $longitude . '&sensor=false&language=ja'), true);
-
-//  var_dump($GMADs);
-/*
-** 住所取得
-*/
-// $addresses = array();
- 
-    // var_dump($GMAD);
- $address = $GMAD['results'][0]['formatted_address'];
+$address = $GMAD['results'][0]['formatted_address'];
 
 $address = preg_replace("/日本、〒[0-9]{3}-[0-9]{4} /","",$address);
 return $address;
