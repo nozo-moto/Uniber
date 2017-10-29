adc6837f6c9b77b5d5c3a9b3fe4d13288f0aa53e
Your Name <nozomotoitech@gmail.com>
2017-10-29 14:18:38 +0900

    update

--- func_conv_addr.php
+++ func_conv_addr.php
@@ -1,13 +1,16 @@
 <?php
 
 //座標値を住所に変換
-function conv_addr($latitude,$longitude){
+
+
+function conv_addr($longitude, $latitude){
 $GMAD = json_decode(@file_get_contents('http://maps.google.com/maps/api/geocode/json?latlng=' . 
     $latitude . ',' . $longitude . '&sensor=false&language=ja'), true);
 
 $address = $GMAD['results'][0]['formatted_address'];
 
 $address = preg_replace("/日本、〒[0-9]{3}-[0-9]{4} /","",$address);
+// print($address);
 return $address;
 }
 ?>
