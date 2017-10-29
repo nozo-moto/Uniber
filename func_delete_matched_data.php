fc5f637802b9cf1a45f12fafd614974ec70f9508
Nozomi Morimoto <nozomotoitech@gmail.com>
2017-10-29 14:46:39 +0900

    Update func_delete_matched_data.php

--- func_delete_matched_data.php
+++ func_delete_matched_data.php
@@ -3,6 +3,6 @@
 //座標値を住所に変換
 function func_delete_matched_data($supply_car_id){
     $db = new PDO('sqlite:db/uniber.db');
-    $run=$db->query("delete from supply_car where id = '$supply_car_id';");
+    $run=$db->query("delete from supply_car where id = '$supply_car_id'");
 }
 ?>
